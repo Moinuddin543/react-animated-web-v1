76e6377ac19a12b8f7866a1f61ee5732e3c31426
moin uddin <moinuddincr77@gmail.com>
2021-09-10 20:13:55 +0600

    react-animated-web project finished

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+         <footer className="bg-info text-center">
+            <small><p>© 2021 Moin uddin. All Rights Reserved | Terms and Conditions.</p></small>
+         </footer>
+            
+        </>
+    );
+};
+
+export default Footer;
